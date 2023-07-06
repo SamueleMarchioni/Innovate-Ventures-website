91b07a6b363a00e755b8b51f19a93c8ac1f692a0
SamueleMarchioni <marchio99sammi@gmail.com>
2023-07-06 13:38:50 +0200

    adjustments

--- progetto/frontend/.nuxt/types/middleware.d.ts
+++ progetto/frontend/.nuxt/types/middleware.d.ts
@@ -1,6 +1,6 @@
 import type { NavigationGuard } from 'vue-router'
 export type MiddlewareKey = string
-declare module "/Users/pippomonti99/Desktop/Hypermedia Project/progetto/frontend/node_modules/nuxt/dist/pages/runtime/composables" {
+declare module "/Users/samuele.marchioni/Desktop/u/progetto/frontend/node_modules/nuxt/dist/pages/runtime/composables" {
   interface PageMeta {
     middleware?: MiddlewareKey | NavigationGuard | Array<MiddlewareKey | NavigationGuard>
   }
